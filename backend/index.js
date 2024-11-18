1f8a20313d81f157fd71f9b6018eb7c8d95d5a10
Rahul Saxena <rahulsaxenaa2002@gmail.com>
2024-11-19 00:19:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use(cors({ origin: "https://google-ks5wl3rwp-rahul-saxenas-projects.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://google-hjjsoqp3k-rahul-saxenas-projects.vercel.app", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.json());
